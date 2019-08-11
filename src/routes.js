ee0e00e4512bff01dd2de67f663c2b2e032333bf
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-11 18:02:55 -0300

    Head partial view and main css file

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.send('Hello World')
+  return res.render('auth/signup')
 })
 
 module.exports = routes
